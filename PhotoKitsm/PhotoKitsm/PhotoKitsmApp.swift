ccb089811eb763b08aed16d00ca1905e7a9a3df2
norang <norang1221@gmail.com>
2024-06-17 11:18:49 +0900

    git init

---
+++ PhotoKitsm/PhotoKitsm/PhotoKitsmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoKitsmApp.swift
+//  PhotoKitsm
+//
+//  Created by Kyuhee hong on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoKitsmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
