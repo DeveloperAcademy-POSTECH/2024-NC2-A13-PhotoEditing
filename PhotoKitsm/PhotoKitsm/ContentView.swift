c6fb6fc6983494cf17e7290d9433ab2f213e5e01
norang <norang1221@gmail.com>
2024-06-17 11:29:41 +0900

    text added

--- PhotoKitsm/PhotoKitsm/ContentView.swift
+++ PhotoKitsm/PhotoKitsm/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "photo")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("PhotoKitsm-is-here")
         }
         .padding()
     }
